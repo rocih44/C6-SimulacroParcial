b3d01c899f2d7dac90aa6321d1d958d84fbc75fb
Rocio <rociohuni23@gmail.com>
2025-05-08 20:16:08 -0300

    Try,Catch

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ const app= e();
 app.use(e.json());
 
 app.post('/operaciones', (req,res)=>{
+try{
     const resul= req.body.map(e=>{
         const funcion= func.find(o=>o.name == e.fn);
         return {...e,resultado: funcion.value(e.op1, e.op2)}
     })
-    res.status(201).json(resul)
+    res.status(201).json(resul);
+}catch(e){
+    res.status(500).json({mensaje: e.message})}
 });
 
 app.listen(5000,()=>{
